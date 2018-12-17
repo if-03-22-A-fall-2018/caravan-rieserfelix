e04dd5799c4358760998784be487e49cf0e5bdd0
Felix Rieser <dafelix.rieser@gmail.com>
2018-12-17 13:34:26 +0100

    first push

--- caravan.cpp
+++ caravan.cpp
@@ -10,34 +10,46 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+
+#include "caravan.h"
+
+
 Caravan new_caravan()
 {
+  return 0;
 }
 
 int get_length(Caravan caravan)
 {
+  return 0;
 }
 
 void delete_caravan(Caravan caravan)
 {
+
 }
 
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
+
 }
 
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
 {
+
 }
 
 int get_caravan_load(Caravan caravan)
 {
+  return 0;
 }
 
 void unload(Caravan caravan)
 {
+
 }
 
 int get_caravan_speed(Caravan caravan)
 {
+  return 0;
 }
